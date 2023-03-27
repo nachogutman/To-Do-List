336b589c954a1a50034160895d376f9dd1ecccc2
nachogutman <nachogutmann@gmail.com>
2023-03-27 20:01:28 -0300

    Avances

--- site.js
+++ site.js
@@ -1,37 +1,49 @@
-const tareas = document.getElementById('tareas');
+const tareas = document.getElementById("tareas");
 var arrayTareas = [];
 
-function agregar(){
-    let texto = document.getElementById('texto');
-    const tarea = {
-        id: arrayTareas.length,
-        texto: texto.value,
-        fechaCreacion: Date.now(),
-    }
+function agregar() {
+  //crea elemento
+  let texto = document.getElementById("texto");
+  const tarea = {
+    id: arrayTareas.length,
+    texto: texto.value,
+    fechaCreacion: Date.now(),
+    check: false,
+  };
 
-    tareas.innerHTML += `
+  //vacia lista
+  tareas.innerHTML = "";
+
+  //agrega elemento a array
+  arrayTareas.push(tarea);
+  
+  arrayTareas.map((t) => {
+    if (t.check == true) {
+      tareas.innerHTML += `
         <li>
-            <input type="checkbox" class="tarea" id=${tarea.id}> <h5 id=${tarea.texto}> ${texto.value} </h5> 
+            <input type="checkbox" class="tarea" id=${t.id} checked> <h5 id=${t.texto}> ${t.texto} </h5> 
         </li>
-    `;
+      `;
+      document.getElementById(t.texto).style = "text-decoration:line-through;";
+    } else {
+      tareas.innerHTML += `
+        <li>
+            <input type="checkbox" class="tarea" id=${t.id}> <h5 id=${t.texto}> ${t.texto} </h5> 
+        </li>
+      `;
+    }
+    
+    let checkbox = document.getElementById(tarea.id);
+    checkbox.addEventListener("change", (e) => {
+      if (e.target.checked) {
+        document.getElementById(tarea.texto).style =
+          "text-decoration:line-through;";
+        arrayTareas[tarea.id].check = true;
 
-    arrayTareas.push(tarea);
-    texto.value = '';
-
-    arrayTareas.map( tarea => {
-        let checkbox = document.getElementById(tarea.id);
-        checkbox.addEventListener('change', e => {
-
-            if(e.target.checked){
-                document.getElementById(tarea.texto).style = "text-decoration:line-through;";  
-            } else {
-                document.getElementById(tarea.texto).style = "text-decoration:none;";  
-            }
-        
-        });
+      } else {
+        document.getElementById(tarea.texto).style = "text-decoration:none;";
+      }
     });
+  });
+
 }
-
-
-
-
